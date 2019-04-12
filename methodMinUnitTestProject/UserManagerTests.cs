329bbbd6d51795b6f5d7f0e6763452d9f9fd86fc
Denis Stepanenko <denis.stepanenko@yahoo.com>
2019-04-12 14:56:16 +0300

    Добавил класс UserManager для написания Data Driven теста в UserMangerTests

---
+++ methodMinUnitTestProject/UserManagerTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace programForUnitTest.Tests
+{
+    [TestClass]
+    public class UserManagerTests
+    {
+        public TestContext TestContext { get; set; }
+        UserManager manager = new UserManager();
+
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.XML", 
+            "TestData.xml", 
+            "User", 
+            DataAccessMethod.Sequential)]
+        [TestMethod]
+        public void Add_FromXML()
+        {
+            // Arrange
+            string username = (string)TestContext.DataRow["username"];
+            string phone = (string)TestContext.DataRow["phone"];
+            string email = (string)TestContext.DataRow["email"];
+
+            // Act
+            try
+            {
+                manager.Add(username, phone, email);
+            } 
+            //Assert
+            catch(Exception ex)
+            {
+                Assert.Fail(ex.Message);
+            }
+        }
+    }
+}
---
+++ programForUnitTest/UserManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace programForUnitTest
+{
+    public class UserManager
+    {
+        public void Add(string username, string phone, string email)
+        {
+            if(!Regex.IsMatch(email, @"^.+@.+\..+$"))
+            {
+                throw new Exception("Некорректный формат e-mail");
+            }
+
+            if(!Regex.IsMatch(username, @"^[а-яА-Яa-zA-Z ]+$"))
+            {
+                throw new Exception("Некорректное имя пользователя");
+            }
+
+            if(!Regex.IsMatch(phone, @"\d{11}"))
+            {
+                throw new Exception("Некорректный номер телефона");
+            }
+        }
+    }
+}
